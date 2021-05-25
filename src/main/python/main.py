4817502d635cbc5083360c13354432219a9ee87c
Roland Kahlert <roland.kahlert@pwc.com>
2021-05-25 23:24:13 +0200

    request data in worker thread

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,8 +1,11 @@
-from PyQt5 import uic
-from PyQt5.QtCore import pyqtSlot
-from PyQt5.QtWidgets import QApplication, QInputDialog, QLineEdit, QWidget
+import traceback
 
-from src.main.python.prestige import association_prestige
+from PyQt5 import uic
+from PyQt5.QtCore import pyqtSlot, QThread
+from PyQt5.QtWidgets import QApplication, QInputDialog, QLineEdit, QWidget, QDialog, QDialogButtonBox, \
+    QVBoxLayout, QLabel, QFileDialog
+
+from src.main.python.prestige import PrestigeWorker
 from src.main.python.server import ServerCaller
 
 Form, Window = uic.loadUiType("../dialog.ui")
@@ -19,13 +22,58 @@ class App(Form, QWidget):
         session_id, _ = QInputDialog.getText(self, "PHPSESSID", "PHPSESSID:", QLineEdit.Normal)
         self.api = ServerCaller(f's{server_number}.railnation.de', session_id)
 
-    @pyqtSlot()
-    def clicked_prestige(self):
-        print('Collecting Prestige information')
-        #self.btn_loads.setEnabled(False)
-        #self.btn_storage.setEnabled(False)
+    def report_progress(self, i, total):
+        self.progressBar.setValue(int(i / total * 100))
 
-        association_prestige(self.api)
+    def finished_process(self):
+        print('Cleanup')
+        self.report_progress(0, 1)
+        self.btn_prestige.setEnabled(True)
+        self.btn_loads.setEnabled(True)
+        self.btn_storage.setEnabled(True)
+
+    def clicked_prestige(self):
+        self.btn_prestige.setEnabled(False)
+        self.btn_loads.setEnabled(False)
+        self.btn_storage.setEnabled(False)
+        filename = QFileDialog.getSaveFileName(self, 'Datei speichern', "prestige.csv")
+
+        try:
+            self.thread = QThread()
+            self.worker = PrestigeWorker(self.api, filename[0])
+            self.worker.moveToThread(self.thread)
+            # Step 5: Connect signals and slots
+            self.thread.started.connect(self.worker.run)
+            self.worker.finished.connect(self.thread.quit)
+            self.worker.finished.connect(self.worker.deleteLater)
+            self.thread.finished.connect(self.thread.deleteLater)
+            self.worker.progress.connect(self.report_progress)
+            self.thread.finished.connect(self.finished_process)
+            # Step 6: Start the thread
+            self.thread.start()
+        except:
+            dlg = ExceptionDialog()
+            dlg.exec()
+
+
+class ExceptionDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('Unhandled Exception')
+
+        self.buttonBox = QDialogButtonBox(QDialogButtonBox.Close)
+        self.buttonBox.clicked.connect(self.close_application)
+
+        self.layout = QVBoxLayout()
+        message = QLabel(traceback.format_exc())
+        self.layout.addWidget(message)
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
+
+    @pyqtSlot()
+    def close_application(self):
+        exit(-1)
 
 
 if __name__ == '__main__':
--- src/main/python/prestige.py
+++ src/main/python/prestige.py
@@ -2,57 +2,73 @@ import csv
 
 from time import sleep
 
+from PyQt5.QtCore import QObject, pyqtSignal
 
-def association_prestige(api):
-    # association analysis
-    assoc_info = api.call('CorporationInterface', 'getOtherMemberBuildings', ["00000000-0000-0000-0000-000000000000"])
-    print(f"Info for {len(assoc_info['Body'])} members")
 
-    # fetch all prestige details
-    prestiges = {}
-    for user_id, member in assoc_info['Body'].items():
-        prestiges[user_id] = {}
-        prestiges[user_id]['overview'] = api.call('BudgetInterface', 'getPrestigeHistoryDetails',
-                                                  [user_id, 9])['Body']['balance']
-        sleep(1)
-        prestiges[user_id]['cities'] = api.call('BudgetInterface', 'getPrestigeHistoryDetails',
-                                                [user_id, 9, '0', '1'])['Body']['balance']
-        sleep(1)
-        prestiges[user_id]['landmarks'] = api.call('BudgetInterface', 'getPrestigeHistoryDetails',
-                                                   [user_id, 9, '0', '2'])['Body']['balance']
-        sleep(2)
+class PrestigeWorker(QObject):
+    finished = pyqtSignal()
+    progress = pyqtSignal(int, int)
 
-    # fetch all profiles for the nick names
-    user_ids = list(prestiges.keys())
-    profiles = api.call('ProfileInterface', 'getVCard', [user_ids], short_call=1236)['Body']
+    def __init__(self, api, filename):
+        super().__init__()
+        self.api = api
+        self.filename = filename
 
-    # prepare all city and landmarks for the csv header line
-    cities = set(city['type'] for user_id, prestige in prestiges.items() for city in prestige['cities'])
-    landmarks = set(landmark['type'] for user_id, prestige in prestiges.items() for landmark in prestige['landmarks'])
+    def run(self):
+        # association analysis
+        assoc_info = self.api.call('CorporationInterface', 'getOtherMemberBuildings',
+                                   ["00000000-0000-0000-0000-000000000000"])
+        print(f"Info for {len(assoc_info['Body'])} members")
 
-    with open('prestige.csv', 'w', newline='') as csvfile:
-        fieldnames = ['member', 'transports', 'invests', 'trainstation', 'other', 'competition', 'medal',
-                      '-cities-'] + list(cities) + ['-landmarks-'] + list(landmarks)
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
+        # fetch all prestige details
+        prestiges = {}
+        i = 0
+        for user_id, member in assoc_info['Body'].items():
+            prestiges[user_id] = {}
+            prestiges[user_id]['overview'] = self.api.call('BudgetInterface', 'getPrestigeHistoryDetails',
+                                                           [user_id, 9])['Body']['balance']
+            sleep(1)
+            prestiges[user_id]['cities'] = self.api.call('BudgetInterface', 'getPrestigeHistoryDetails',
+                                                         [user_id, 9, '0', '1'])['Body']['balance']
+            sleep(1)
+            prestiges[user_id]['landmarks'] = self.api.call('BudgetInterface', 'getPrestigeHistoryDetails',
+                                                            [user_id, 9, '0', '2'])['Body']['balance']
+            sleep(2)
+            i += 1
+            self.progress.emit(i, len(assoc_info['Body']))
 
-        for user_id, prestige in prestiges.items():
-            overview_dict = {
-                'transports': [row for row in prestige['overview'] if row['type'] == '0'],
-                'invests': [row for row in prestige['overview'] if row['type'] == '3'],
-                'trainstation': [row for row in prestige['overview'] if row['type'] == '4'],
-                'other': [row for row in prestige['overview'] if row['type'] == '6'],
-                'competition': [row for row in prestige['overview'] if row['type'] == '7'],
-                'medal': [row for row in prestige['overview'] if row['type'] == '8'],
-            }
-            write_dict = {'member': profiles[user_id]['userName']}
-            for key, value in overview_dict.items():
-                if len(value) > 0:
-                    write_dict[key] = value[0]['prestige']
+        # fetch all profiles for the nick names
+        user_ids = list(prestiges.keys())
+        profiles = self.api.call('ProfileInterface', 'getVCard', [user_ids], short_call=1236)['Body']
 
-            for row in prestige['cities']:
-                write_dict[row['type']] = row['prestige']
-            for row in prestige['landmarks']:
-                write_dict[row['type']] = row['prestige']
+        # prepare all city and landmarks for the csv header line
+        cities = set(city['type'] for user_id, prestige in prestiges.items() for city in prestige['cities'])
+        landmarks = set(landmark['type'] for user_id, prestige in prestiges.items() for landmark in prestige['landmarks'])
 
-            writer.writerow(write_dict)
+        with open(self.filename, 'w', newline='') as csvfile:
+            fieldnames = ['member', 'transports', 'invests', 'trainstation', 'other', 'competition', 'medal',
+                          '-cities-'] + list(cities) + ['-landmarks-'] + list(landmarks)
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+
+            for user_id, prestige in prestiges.items():
+                overview_dict = {
+                    'transports': [row for row in prestige['overview'] if row['type'] == '0'],
+                    'invests': [row for row in prestige['overview'] if row['type'] == '3'],
+                    'trainstation': [row for row in prestige['overview'] if row['type'] == '4'],
+                    'other': [row for row in prestige['overview'] if row['type'] == '6'],
+                    'competition': [row for row in prestige['overview'] if row['type'] == '7'],
+                    'medal': [row for row in prestige['overview'] if row['type'] == '8'],
+                }
+                write_dict = {'member': profiles[user_id]['userName']}
+                for key, value in overview_dict.items():
+                    if len(value) > 0:
+                        write_dict[key] = value[0]['prestige']
+
+                for row in prestige['cities']:
+                    write_dict[row['type']] = row['prestige']
+                for row in prestige['landmarks']:
+                    write_dict[row['type']] = row['prestige']
+
+                writer.writerow(write_dict)
+        self.finished.emit()
